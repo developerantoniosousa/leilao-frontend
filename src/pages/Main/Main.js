e2b4af2a323597834341ea45601c27439188b60a
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-10 09:17:39 -0300

    feat(pages): mount Main view

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-function Main() {
-  return <div />;
+import Container from '../../components/Container';
+import { formatPrice } from '../../util/format';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 600,
+  },
+});
+
+const data = [
+  {
+    id: 1,
+    name: 'Leilão #1',
+    value: formatPrice(1000),
+    is_used: 'Sim',
+    is_completed: 'Não',
+    responsabler: {
+      name: 'Antonio Sousa'
+    }
+  }
+];
+
+export default function Main() {
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell align="right">Valor inicial</TableCell>
+              <TableCell align="right">Usado</TableCell>
+              <TableCell align="right">Responsável</TableCell>
+              <TableCell align="right">Finalizado</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((auction) => (
+              <TableRow key={auction.id}>
+                <TableCell component="th" scope="row">
+                  {auction.name}
+                </TableCell>
+                <TableCell align="right">{auction.value}</TableCell>
+                <TableCell align="right">{auction.is_used}</TableCell>
+                <TableCell align="right">{auction.responsabler.name}</TableCell>
+                <TableCell align="right">{auction.is_completed}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
-
-export default Main;
