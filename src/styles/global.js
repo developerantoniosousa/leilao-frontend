f1faec349b2a4e055cfcb33443b0fcdfdb521658
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-10 08:48:10 -0300

    feat: set up global style

---
+++ src/styles/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #ED9F35;
+    font-family: Arial, Helvetica, sans-serif
+  }
+`;
