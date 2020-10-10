0917063c5c180c0ab05bb2fb4076786dd63ae6d3
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-10 10:43:28 -0300

    feat: authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Routes from './routes';
+import Login from './pages/Login';
 
 function App() {
-  return <Routes />;
+  const [userChecked, setUserChecked] = useState(false);
+  const [userLogged, setUserLogged] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('@leilao:token');
+    if (token) {
+      setUserLogged(true);
+    }
+    setUserChecked(true);
+  }, []);
+
+  function reloadApp() {
+    window.location.reload();
+  }
+
+  if (!userChecked) return null;
+
+  return userLogged ? <Routes /> : <Login reloadApp={reloadApp} />
 }
 
 export default App;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { toast } from 'react-toastify';
 
+import api from '../../services/api';
 import { Container, Form } from './styles';
 
-function Login() {
-  const handleSubmit = (event) => {
+function Login({ reloadApp }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const response = await api.post('sessions', { email, password });
+      localStorage.setItem('@leilao:token', response.data.token);
+      reloadApp();
+    } catch {
+      toast.error('Verifique seus dados e tente novamente');
+    }
   };
 
   return (
     <Container center>
       <Form onSubmit={handleSubmit}>
         <h1>Faça login</h1>
-        <TextField type="email" label="E-mail" size="small" />
-        <TextField type="password" label="Senha" />
-        <Button variant="contained" color="secondary">
+        <TextField
+          type="email"
+          label="E-mail"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+        <TextField
+          type="password"
+          label="Senha"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        <Button type="submit" variant="contained" color="secondary">
           Entrar
         </Button>
       </Form>
