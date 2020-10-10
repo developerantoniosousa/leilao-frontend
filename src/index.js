4fbd1f597408d8e4b8a81a4f153973a268513ffa
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-10 10:27:29 -0300

    feat: set up  react-toastify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ToastContainer } from 'react-toastify';
+
 import App from './App';
 import GlobalStyle from './styles/global';
 
@@ -7,6 +9,7 @@ ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
+    <ToastContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
