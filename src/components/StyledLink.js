f8898270e1316975457a77ac5dbd43d4ffed7743
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-10 11:20:16 -0300

    feat(components): add StyledLink

---
+++ src/components/StyledLink.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const StyledLink = styled(Link)`
+  margin-bottom: 16px;
+  text-decoration: none;
+  font-weight: 500;
+  display: block;
+  text-align: center;
+`;
+
+export default StyledLink;
