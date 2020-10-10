3ff6684fef9a8d91e2a8c94444801129883bfa02
Antonio Sousa <developerantoniosousa@gmail.com>
2020-10-10 11:37:02 -0300

    feat: creating auction

--- src/pages/Auction/Add.js
+++ src/pages/Auction/Add.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
+import { toast } from 'react-toastify';
 
 import Container from '../../components/Container';
+import StyledLink from '../../components/StyledLink';
 import { Form } from './styles';
+import api from '../../services/api';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,20 +21,47 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function BasicTextFields() {
+export default function AddAuction({ history }) {
+  const [name, setName] = useState('');
+  const [value, setValue] = useState(0);
+  const [isUsed, setIsUsed] = useState(false);
   const classes = useStyles();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      await api.post('auctions', {
+        is_used: isUsed,
+        name,
+        value,
+      });
+      toast.success('Leilão cadastrado');
+      history.goBack();
+    } catch {
+      toast.error('Houve algum problema, verifique os dados e tente novamente');
+    }
+  }
 
   return (
     <Container center>
-      <Form className={classes.root} noValidate autoComplete="off">
-        <TextField label="Nome" />
-        <TextField label="Valor inicial" />
+      <Form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <StyledLink to="/">Voltar para os leilões</StyledLink>
+        <TextField
+          label="Nome"
+          value={name}
+          onChange={event => setName(event.target.value)}
+        />
+        <TextField
+          label="Valor inicial"
+          value={value}
+          onChange={event => setValue(event.target.value)}
+        />
         <FormGroup row>
           <FormControlLabel
             control={
               <Checkbox
-                checked={true}
-                onChange={() => { }}
+                checked={isUsed}
+                onChange={() => setIsUsed(state => !state)}
                 name="is_used"
                 color="primary"
               />
@@ -39,7 +69,7 @@ export default function BasicTextFields() {
             label="Produto usado"
           />
         </FormGroup>
-        <Button variant="contained" color="secondary">
+        <Button type="submit" variant="contained" color="secondary">
           Cadastrar
         </Button>
       </Form>
